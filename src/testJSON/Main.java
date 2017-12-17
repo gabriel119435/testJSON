6c97d612eba1cbb3a219c9819f80be5dab076ca9
Gabriel Rado <gabriel119435@gmail.com>
2017-12-17 00:07:42 -0200

    minor changes

--- src/testJSON/Main.java
+++ src/testJSON/Main.java
@@ -3,15 +3,12 @@ package testJSON;
 import com.google.gson.Gson;
 
 public class Main {
-
 	public static void main(String[] args) {
-
 		Gson gson = new Gson();
 		Obj obj = new Obj();
 		obj.setNome("gabriel");
 		obj.setIdade(23);
 		System.out.println(gson.toJson(obj));
-		System.out.println(   gson.fromJson(gson.toJson(obj),Obj.class)     );
+		System.out.println(gson.fromJson(gson.toJson(obj), Obj.class));
 	}
-
 }
